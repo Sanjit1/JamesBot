d572c045bf5ab45e47184b52384de0071bbdb1c4
LegandDairy <34670497+leonlenk@users.noreply.github.com>
2022-04-21 23:27:16 -0600

    Update counting.js

--- modules/counting.js
+++ modules/counting.js
@@ -1,5 +1,30 @@
 // why not: https://discord.js.org/#/docs/discord.js/
 
-const handle = (message) => {};
+var currentCount = 1;
+var mostRecentUser = "";
+
+const handle = (message) => {
+    if ((message.content == (currentCount)) && (message.author.id != mostRecentUser)){
+        currentCount ++;
+        message.react("☑️");
+        mostRecentUser = message.author.id
+    }else if(!isNaN(message.content)){
+        message.react("❎");
+        if (currentCount <= 1){
+            currentCount = 1
+            mostRecentUser = ""
+        }else if(currentCount <= 50){
+            currentCount = currentCount - 1
+        }else if(currentCount <= 100){
+            currentCount = Math.floor(currentCount * .95)
+        }else{
+            currentCount = Math.floor(currentCount * .9)
+        }
+        if (mostRecentUser == message.author.id){
+            message.channel.send("You can't count twice in a row!")
+        }
+        message.channel.send("<@" + message.author.id + ">" + " sucks. " + "The next number is: " + (currentCount))
+    }
+}; 
 
 module.exports = { handle };
