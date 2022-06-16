34bfe65cb41f28c93da5dfb232b0e3e46ad5f2dc
Sanjit Sarda <sanjitsarda4@gmail.com>
2022-06-16 10:38:14 -0700

    Update topicPings.js

--- modules/topicPings.js
+++ modules/topicPings.js
@@ -8,79 +8,92 @@ const handle = (message, MessageEmbed) => {
     parsedStorage = JSON.parse(storage);
     var filter = /[a-zA-Z0-9 -!@#$% ^&*:;,.~+-=]/gm; // Filter out some characters
     Object.keys(parsedStorage.modules.pings.users).forEach((element) => {
-        if (
-            message.guild.member(
-                element
-            )
-        ) {
-            var memberToPing = await message.guild.members.fetch(
-                element
-            );
-        
-
-        if (
-            message.author.id != element &&
-            !onCoolDown.has(element) &&
-            message.channel.permissionsFor(element).has("READ_MESSAGE_HISTORY")
-        ) {
-            parsedStorage.modules.pings.users[element].forEach((topic) => {
-                simplified = (message.content.match(filter) || []).join("");
+        async () => {
+            if (message.guild.member(element)) {
+                var memberToPing = await message.guild.members.fetch(element);
                 if (
-                    simplified.split(" ").includes(topic) ||
-                    simplified.replace(".", "").split(" ").includes(topic) ||
-                    simplified.replace(",", "").split(" ").includes(topic) ||
-                    simplified.replace('"', "").split(" ").includes(topic) ||
-                    simplified.replace("'", "").split(" ").includes(topic)
+                    message.author.id != element &&
+                    !onCoolDown.has(element) &&
+                    message.channel
+                        .permissionsFor(element)
+                        .has("READ_MESSAGE_HISTORY")
                 ) {
-                    var pingEmbed = new MessageEmbed()
-                        .setColor(
-                            "#" +
-                            Math.floor(Math.random() * 16777215).toString(
-                                16
-                            )
-                        ) // set a random color cuz why not
-                        .setAuthor({
-                            name: message.author.username,
-                            iconURL: message.author.displayAvatarURL(),
-                        }) // show the author of the original message on the embed
-                        .addFields(
-                            {
-                                name: "Topic `" + topic + "` found",
-                                value:
-                                    "[Click here to be teleported](" +
-                                    message.url +
-                                    ")",
-                                inline: true,
-                            },
-                            {
-                                name: "Content",
-                                value: simplified,
-                                inline: true,
-                            },
-                            {
-                                name: "Unsubscribe?",
-                                value:
-                                    "type `j!pings rem " +
-                                    topic +
-                                    "` in the bots channel",
-                                inline: true,
-                            }
-                        ); // add the message content to the historical message embed
+                    parsedStorage.modules.pings.users[element].forEach(
+                        (topic) => {
+                            simplified = (
+                                message.content.match(filter) || []
+                            ).join("");
+                            if (
+                                simplified.split(" ").includes(topic) ||
+                                simplified
+                                    .replace(".", "")
+                                    .split(" ")
+                                    .includes(topic) ||
+                                simplified
+                                    .replace(",", "")
+                                    .split(" ")
+                                    .includes(topic) ||
+                                simplified
+                                    .replace('"', "")
+                                    .split(" ")
+                                    .includes(topic) ||
+                                simplified
+                                    .replace("'", "")
+                                    .split(" ")
+                                    .includes(topic)
+                            ) {
+                                var pingEmbed = new MessageEmbed()
+                                    .setColor(
+                                        "#" +
+                                            Math.floor(
+                                                Math.random() * 16777215
+                                            ).toString(16)
+                                    ) // set a random color cuz why not
+                                    .setAuthor({
+                                        name: message.author.username,
+                                        iconURL:
+                                            message.author.displayAvatarURL(),
+                                    }) // show the author of the original message on the embed
+                                    .addFields(
+                                        {
+                                            name: "Topic `" + topic + "` found",
+                                            value:
+                                                "[Click here to be teleported](" +
+                                                message.url +
+                                                ")",
+                                            inline: true,
+                                        },
+                                        {
+                                            name: "Content",
+                                            value: simplified,
+                                            inline: true,
+                                        },
+                                        {
+                                            name: "Unsubscribe?",
+                                            value:
+                                                "type `j!pings rem " +
+                                                topic +
+                                                "` in the bots channel",
+                                            inline: true,
+                                        }
+                                    ); // add the message content to the historical message embed
 
-                    memberToPing
-                        .send({ embeds: [pingEmbed] })
-                        .catch(() => { });
-                    wrapper = memberToPing.id;
-                    onCoolDown.add(wrapper);
-                    setTimeout(() => {
-                        onCoolDown.delete(wrapper);
-                    }, 1 * 1000 * 3);
+                                memberToPing
+                                    .send({ embeds: [pingEmbed] })
+                                    .catch(() => {});
+                                wrapper = memberToPing.id;
+                                onCoolDown.add(wrapper);
+                                setTimeout(() => {
+                                    onCoolDown.delete(wrapper);
+                                }, 1 * 1000 * 3);
+                            }
+                        }
+                    );
                 }
-            });
-        }
-    } else {
-        //delete later when not lazy
-    }
+            } else {
+                //delete later when not lazy
+            }
+        };
     });
 };
 
